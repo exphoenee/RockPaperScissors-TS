3f5bd239882eee39d7c78dd2b796d8fe3d1b56c8
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-05 17:41:05 +0100

    Create getAppElements.ts

---
+++ src/utils/getAppElements.ts
@@ -0,0 +1,18 @@
+import appElements, { appElementType } from "../constants/appElements";
+
+export default function getAppElements(): (Element | Element[] | null)[] {
+  const domElems = appElements.reduce((acc: Object, el: appElementType) => {
+    if (el.id) {
+      acc[el.name] = document.getElementById(el.id);
+    } else if (el.class) {
+      acc[el.name] = document.querySelector(el.class);
+    } else if (el.classes) {
+      acc[el.name] = Array.from(document.querySelectorAll(el.classes));
+    } else {
+      console.error(el.name, "is missing!");
+    }
+    return acc;
+  }, {});
+  console.log(domElems);
+  return domElems;
+}
