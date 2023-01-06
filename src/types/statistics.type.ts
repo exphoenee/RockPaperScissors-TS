6956159a7e2067b32057e243899af85f6615fde1
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-06 12:53:42 +0100

    Update statistics.type.ts

--- src/types/statistics.type.ts
+++ src/types/statistics.type.ts
@@ -1,6 +1,15 @@
-type statisticsType = {
-  player: {};
-  computer: {};
+export type gameStatisticsType = {
+  name: string;
+  values: {
+    opponent: {
+      [key: string]: number;
+    };
+    player: {
+      [key: string]: number;
+    };
+  };
 };
+
+type statisticsType = gameStatisticsType[];
 
 export default statisticsType;
