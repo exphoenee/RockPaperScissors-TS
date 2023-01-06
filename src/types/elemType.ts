f9fbadbad291bd2ebe887d23ae58cff90209dec9
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-06 15:09:37 +0100

    Delete elemType.ts

--- src/types/elemType.ts
+++
@@ -1,9 +0,0 @@
-export type singleElemType = { [key: string]: Element };
-export type multiElemType = { [key: string]: Element[] };
-
-type elemType = {
-  single: singleElemType;
-  multi: multiElemType;
-};
-
-export default elemType;
