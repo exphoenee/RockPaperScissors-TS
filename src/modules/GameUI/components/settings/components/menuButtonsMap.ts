66b91611d0e1506c7a595b630272bd8bcfc0ed81
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-07 18:10:42 +0100

    Create menuButtonsMap.ts

---
+++ src/modules/GameUI/components/settings/components/menuButtonsMap.ts
@@ -0,0 +1,27 @@
+import { buttonMapType } from "../../common/buttonMap";
+
+const menuButtonsMap: buttonMapType[] = [
+  {
+    className: ["rules"],
+    fileName: "media/rules.png",
+  },
+  {
+    className: ["statistics"],
+    fileName: "media/statistics.png",
+  },
+  {
+    className: ["language"],
+    fileName: "media/language.png",
+  },
+  {
+    className: ["licensing"],
+    fileName: "media/language.png",
+  },
+  {
+    className: ["thema"],
+    imageClass: ["sun on", "moon off"],
+    fileName: ["media/sunny.png", "media/moon.png"],
+  },
+];
+
+export default menuButtonsMap;
