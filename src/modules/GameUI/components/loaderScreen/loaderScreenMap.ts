de4ffbe04d536e933bca14efc912786ded853e6c
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-07 17:58:56 +0100

    Create loaderScreenMap.ts

---
+++ src/modules/GameUI/components/loaderScreen/loaderScreenMap.ts
@@ -0,0 +1,11 @@
+import loaderImageMap from "./components/loaderImageMap";
+
+const loaderScreenMap = {
+  tag: "div",
+  attrs: {
+    id: "loader-screen",
+  },
+  children: loaderImageMap,
+};
+
+export default loaderScreenMap;
