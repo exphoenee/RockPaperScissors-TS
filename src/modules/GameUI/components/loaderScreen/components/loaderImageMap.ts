3053c8008d783b983e47d46df09fdee05a17e40a
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-07 17:58:52 +0100

    Create loaderImageMap.ts

---
+++ src/modules/GameUI/components/loaderScreen/components/loaderImageMap.ts
@@ -0,0 +1,16 @@
+const loaderImageMap = [
+  {
+    tag: "img",
+    attrs: {
+      id: "loader-image",
+      // class: "loader-image",
+      src: "./media/loader.png",
+      dataset: {
+        filename: "media/loader.png",
+      },
+      alt: "loader",
+    },
+  },
+];
+
+export default loaderImageMap;
