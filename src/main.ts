453c0001170691fce8c00f712f3eb6629c6a7a80
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-07 17:58:38 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
-import Game from "./Game";
+// import Game from "./modules/Game";
+import GameUI from "./modules/GameUI/GameUI";
 
-const game = new Game();
+//const game = new Game();
+new GameUI();
