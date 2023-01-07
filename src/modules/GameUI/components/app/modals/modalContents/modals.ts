cc36c5de2f34eb96508bb1d2ed221cd850d6a5c0
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-07 22:05:41 +0100

    Create modals.ts

---
+++ src/modules/GameUI/components/app/modals/modalContents/modals.ts
@@ -0,0 +1,51 @@
+import rulesModal from "./rulesModal";
+
+const modals = [
+  {
+    name: "rules",
+    title: "Rules",
+    content: rulesModal,
+  },
+  {
+    name: "result",
+    title: "Results",
+    content: {
+      tag: "p",
+      text: "Results",
+    },
+  },
+  {
+    name: "language",
+    title: "language",
+    content: {
+      tag: "p",
+      text: "language",
+    },
+  },
+  {
+    name: "statistics",
+    title: "statistics",
+    content: {
+      tag: "p",
+      text: "statistics",
+    },
+  },
+  {
+    name: "licensing",
+    title: "licensing",
+    content: {
+      tag: "p",
+      text: "licensing",
+    },
+  },
+  {
+    name: "gamemode",
+    title: "gamemode",
+    content: {
+      tag: "p",
+      text: "gamemode",
+    },
+  },
+];
+
+export default modals;
