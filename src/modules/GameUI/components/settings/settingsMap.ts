97aaccce9d46b19c8ea272399880a6bd9d976ef6
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-07 18:10:51 +0100

    Update settingsMap.ts

--- src/modules/GameUI/components/settings/settingsMap.ts
+++ src/modules/GameUI/components/settings/settingsMap.ts
@@ -1,33 +1,7 @@
 import buttonMap from "../common/buttonMap";
 
-const settingButtonMap = {
-  className: ["settings"],
-  fileName: "media/settings.png",
-};
-
-const menuButtonsMap = [
-  {
-    className: ["rules"],
-    fileName: "media/rules.png",
-  },
-  {
-    className: ["statistics"],
-    fileName: "media/statistics.png",
-  },
-  {
-    className: ["language"],
-    fileName: "media/language.png",
-  },
-  {
-    className: ["licensing"],
-    fileName: "media/language.png",
-  },
-  {
-    className: ["thema"],
-    imageClass: ["sun on", "moon off"],
-    fileName: ["media/sunny.png", "media/moon.png"],
-  },
-];
+import settingButtonMap from "./components/settingButtonMap";
+import menuButtonsMap from "./components/menuButtonsMap";
 
 const settingMenuMap = {
   tag: "div",
