65ee5d768425b2b8ce872cba2191083f68ecdaa5
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-07 22:06:48 +0100

    Update GameUI.ts

--- src/modules/GameUI/GameUI.ts
+++ src/modules/GameUI/GameUI.ts
@@ -3,13 +3,25 @@ import loaderScreenMap from "./components/loaderScreen/loaderScreenMap";
 import settingsMap from "./components/settings/settingsMap";
 import appMap from "./components/app/appMap";
 
-export default class GameUI {
-  public loaderScreen: Element;
-  public settings: Element;
+export type GameUIType = {
+  user?: string;
+  opponent?: string;
+};
 
-  constructor() {
-    this.loaderScreen = domelemjs(loaderScreenMap);
-    this.settings = domelemjs(settingsMap);
-    domelemjs(appMap);
+export default class GameUI {
+  // public loaderScreen: Element;
+  // public settings: Element;
+
+  public user?: string;
+  public opponent?: string;
+
+  constructor(props?: GameUIType) {
+    this.user = props?.user || "user";
+    this.opponent = props?.opponent || "opponent";
+
+    // this.loaderScreen = domelemjs(loaderScreenMap);
+    // this.settings =
+    domelemjs(settingsMap);
+    domelemjs(appMap({ user: this.user, opponent: this.opponent }));
   }
 }
