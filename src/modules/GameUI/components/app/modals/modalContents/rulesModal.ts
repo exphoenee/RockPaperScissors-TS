10005a3bcbe738273aac6c337b1ca27987badee9
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-07 22:05:47 +0100

    Create rulesModal.ts

---
+++ src/modules/GameUI/components/app/modals/modalContents/rulesModal.ts
@@ -0,0 +1,18 @@
+import imageMap from "../../../common/imageMap";
+
+const rulesModal = [
+  {
+    tag: "p",
+    text: "Rules",
+    attrs: {
+      id: "rules",
+    },
+  },
+  imageMap({
+    className: "rules",
+    alt: "rules",
+    fileName: "media/rulesdesc.png",
+  }),
+];
+
+export default rulesModal;
