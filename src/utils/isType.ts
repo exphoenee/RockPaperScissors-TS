9404e30f06ce7f00ccf38e31fd55ddcff1b005e4
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-09 16:19:29 +0100

    Update isType.ts

--- src/utils/isType.ts
+++ src/utils/isType.ts
@@ -1,5 +1,8 @@
 export default function isType<T>(obj: any, type: T): obj is T {
-  const keys = Object.keys(type);
+  if (typeof obj !== "object") {
+    return false;
+  }
+  const keys = Object.keys(type as object);
   for (const key of keys) {
     if (obj[key] === undefined) {
       return false;
