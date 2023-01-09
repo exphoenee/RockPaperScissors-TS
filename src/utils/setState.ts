d0a2444290d4c1c20b3f47107a287ba7c928f2e5
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-09 16:42:42 +0100

    Update setState.ts

--- src/utils/setState.ts
+++ src/utils/setState.ts
@@ -1,5 +1,14 @@
-const setState = (state: any, newState: any): void => {
-  localStorage.setItem(state, JSON.stringify(newState));
+const setState = (stateKey: any, newState: any): void => {
+  if (!stateKey) {
+    throw new Error("No state provided");
+  }
+  if (!newState) {
+    throw new Error("No new state provided");
+  }
+  if (typeof newState !== "string") {
+    newState = JSON.stringify(newState);
+  }
+  localStorage.setItem(stateKey, newState);
 };
 
 export default setState;
