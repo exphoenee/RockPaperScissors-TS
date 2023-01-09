157efd4a17ff731d76cd75cfabbc666194ee0a81
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-09 16:54:37 +0100

    Update modalsMap.ts

--- src/modules/GameUI/components/app/modals/modalsMap.ts
+++ src/modules/GameUI/components/app/modals/modalsMap.ts
@@ -1,10 +1,9 @@
-import domelemjs from "domelemjs";
 
 import modalMap from "../../common/modal/modalMap";
 import modals from "./modalContents/modals";
 
-const modalsMap = modals.map((modal) => {
-  return { name: modal.name, element: domelemjs(modalMap(modal)) };
-});
+const modalsMap = modals.map((modal) =>
+  modalMap(modal)
+);
 
 export default modalsMap;
