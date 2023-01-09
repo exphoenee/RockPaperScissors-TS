509923c5f9f11c7c5712e0ec314e0081810c4c0a
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-09 17:52:47 +0100

    Update buttonMap.ts

--- src/modules/GameUI/components/common/buttonMap.ts
+++ src/modules/GameUI/components/common/buttonMap.ts
@@ -4,14 +4,21 @@ export type buttonMapType = {
   className: string[];
   fileName: string | string[];
   imageClass?: string | string[];
+  handleEvent?: { event: ""; cb: () => void } | undefined;
 };
 
-const buttonMap = ({ className, fileName, imageClass = "" }: buttonMapType) => {
+const buttonMap = ({
+  className,
+  fileName,
+  imageClass = "",
+  handleEvent,
+}: buttonMapType) => {
   return {
     tag: "button",
     attrs: {
       class: className && `button ${className.join(" ")}`,
     },
+    handleEvent,
     children: Array.isArray(fileName)
       ? fileName.map((fn, i) => buttonImageMap(fn, className[0], imageClass[i]))
       : buttonImageMap(fileName, className[0], imageClass as string),
