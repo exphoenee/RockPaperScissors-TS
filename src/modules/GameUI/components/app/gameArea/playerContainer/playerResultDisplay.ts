0f64b066caa043acdee2e5b3b0d6d3fbf6491898
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-10 18:50:38 +0100

    Update playerResultDisplay.ts

--- src/modules/GameUI/components/app/gameArea/playerContainer/playerResultDisplay.ts
+++ src/modules/GameUI/components/app/gameArea/playerContainer/playerResultDisplay.ts
@@ -1,9 +1,12 @@
+import dictionary from "../../../../../../constants/dictionary";
+
 const playerResultDisplay = (user: string) => {
   return {
     tag: "p",
     text: "0",
     attrs: {
       class: `${user}-wins`,
+      dataset: { dictionary: `${user}Wins` },
     },
   };
 };
