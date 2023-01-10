667950d455f29be2134ab10c488a3377f9166b45
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-10 18:35:38 +0100

    Update modalMap.ts

--- src/modules/GameUI/components/common/modal/modalMap.ts
+++ src/modules/GameUI/components/common/modal/modalMap.ts
@@ -5,13 +5,15 @@ export type modalMapType = {
   name: string;
   title: string;
   content: object | object[];
+  dictionary?: string;
 };
 
-const modalMap = ({ name, title, content }: modalMapType) => {
+const modalMap = ({ name, title, content, dictionary }: modalMapType) => {
   return {
     tag: "div",
     attrs: {
       class: ["modal", name].join(" "),
+      dataset: { dictionary },
     },
     children: [modalHeaderMap({ title }), modalBodyMap({ content })],
   };
