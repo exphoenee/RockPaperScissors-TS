b5e44977711695af160a58e3d79a9b2966669bab
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-11 17:51:00 +0100

    Create modalNames.ts

---
+++ src/modules/GameUI/constants/modalNames.ts
@@ -0,0 +1,10 @@
+enum modalNames {
+  RULES = "rules-modal",
+  RESULT = "result-modal",
+  LANG = "language-modal",
+  STAT = "statistics-modal",
+  LICENC = "licensing-modal",
+  GAMEMODE = "gamemode-modal",
+}
+
+export default modalNames;
