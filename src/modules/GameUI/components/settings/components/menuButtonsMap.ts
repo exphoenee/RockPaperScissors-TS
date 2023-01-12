01c1bbbfc1ef87a814dad678bc412ebf7c8009f8
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-12 17:58:17 +0100

    Update menuButtonsMap.ts

--- src/modules/GameUI/components/settings/components/menuButtonsMap.ts
+++ src/modules/GameUI/components/settings/components/menuButtonsMap.ts
@@ -24,6 +24,7 @@ const menuButtonsMap: buttonMapType[] = [
   },
   {
     className: ["thema"],
+    id: "thema-button",
     imageClass: ["sun on", "moon off"],
     fileName: ["sunny.png", "moon.png"],
   },
