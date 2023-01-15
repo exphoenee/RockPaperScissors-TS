a1af2051f1673799ba07bf6dffd0f8f834a2e4e8
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-15 15:01:40 +0100

    Update getGameMode.ts

--- src/utils/getGameMode.ts
+++ src/utils/getGameMode.ts
@@ -1,6 +1,17 @@
 import appStates from "../constants/appStates";
 import getState from "./getState";
+import { gameNames } from "../types/gameType";
+import setState from "./setState";
 
-const getGameMode = () => getState(appStates.GAMEMODE);
+const getGameMode = () => {
+  const defaultMode = Object.values(gameNames)[0];
+  const gameMode = getState(appStates.GAMEMODE);
+  if (gameMode) {
+    return getState(appStates.GAMEMODE);
+  } else {
+    setState(appStates.GAMEMODE, defaultMode);
+    return defaultMode;
+  }
+};
 
 export default getGameMode;
