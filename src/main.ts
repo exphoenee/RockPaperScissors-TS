d34dbbd9303b2114b2c37ecad52526477f6ab11b
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-15 14:21:23 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 // import Game from "./modules/Game";
 import GameUI from "./modules/GameUI/GameUI";
 
+/* style */
+import "./style/reset.css";
+import "./style/style.css";
+
 //const game = new Game();
 new GameUI();
