d76b58148e05cc78eab621c962867e23aa66910f
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-19 20:31:20 +0100

    Update gameAreaMap.ts

--- src/modules/GameUI/components/app/gameArea/gameAreaMap.ts
+++ src/modules/GameUI/components/app/gameArea/gameAreaMap.ts
@@ -1,16 +1,18 @@
 import playerContainer from "./playerContainer/playerContainer";
 import vsImageContainer from "./vsImageContainer/vsImageContainer";
+import ruleType from "../../../../../types/ruleType";
+import { playerNames } from "../../../../../types/playerName";
 
-const gameAreaMap = () => {
+const gameAreaMap = ({ rules }: { rules: ruleType[] }) => {
   return {
     tag: "div",
     attrs: {
       class: "game-area",
     },
     children: [
-      playerContainer("user"),
+      playerContainer({ user: playerNames.USER, rules }),
       vsImageContainer,
-      playerContainer("opponent"),
+      playerContainer({ user: playerNames.OPPONENT, rules }),
     ],
   };
 };
