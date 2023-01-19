b76fca721f7679258fa1850503478c07dec21de3
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-19 18:19:34 +0100

    Delete setStatMode.ts

--- src/utils/setStatMode.ts
+++
@@ -1,6 +0,0 @@
-import appStates from "../constants/appStates";
-import setState from "./setState";
-
-const setStateMode = (value: string) => setState(appStates.STATMODE, value);
-
-export default setStateMode;
