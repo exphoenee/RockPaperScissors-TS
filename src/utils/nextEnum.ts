f4297816feb5ec4bdd50d0ce1def635c617b5c15
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-19 19:14:52 +0100

    Delete nextEnum.ts

--- src/utils/nextEnum.ts
+++
@@ -1,9 +0,0 @@
-export default function nextEnum<T extends number>(
-  current: T,
-  enumType: any
-): T {
-  const keys = Object.keys(enumType).filter((k) => isNaN(Number(k)));
-  const max = Number(keys[keys.length - 1]);
-  const next = current + 1 > max ? enumType[keys[0]] : current + 1;
-  return next as T;
-}
