526c852dc43c56704c37b3ec0cdee03a8e786ff0
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-19 20:31:18 +0100

    Update appMap.ts

--- src/modules/GameUI/components/app/appMap.ts
+++ src/modules/GameUI/components/app/appMap.ts
@@ -1,14 +1,15 @@
 import headerMap from "./header/headerMap";
 import gameAreaMap from "./gameArea/gameAreaMap";
 import modalsMap from "./modals/modalsMap";
+import ruleType from "../../../../types/ruleType";
 
-const appMap = () => {
+const appMap = ({ rules }: { rules: ruleType[] }) => {
   return {
     tag: "div",
     attrs: {
       id: "app",
     },
-    children: [headerMap, gameAreaMap(), ...modalsMap],
+    children: [headerMap, gameAreaMap({ rules }), ...modalsMap],
   };
 };
 
