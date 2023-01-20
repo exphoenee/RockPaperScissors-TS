256e6de57e967abf4ce97081b3b5e2d5629ddad0
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-20 15:07:15 +0100

    Update getState.ts

--- src/utils/getState.ts
+++ src/utils/getState.ts
@@ -1,7 +1,8 @@
 const getState = (state: string): any => {
   const value = localStorage.getItem(state);
   if (value) {
-    return value; //JSON.stringify(value);
+    return value;
+    return JSON.stringify(value);
   } else {
     console.error(`No state: ${state} found!`);
     // throw new Error("No state found");
