9358e9221c44f367333bc5bbe96a4caf00877881
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-20 15:07:17 +0100

    Update getThema.ts

--- src/utils/getThema.ts
+++ src/utils/getThema.ts
@@ -1,6 +1,10 @@
 import appStates from "../constants/appStates";
+import themas from "../modules/GameUI/constants/themas";
 import getState from "./getState";
+import checkStrIsInEnum from "./checkStrIsInEnum";
 
-const getThema = () => getState(appStates.THEMA);
+const getThema = () => {
+  return checkStrIsInEnum(getState(appStates.THEMA), themas);
+};
 
 export default getThema;
