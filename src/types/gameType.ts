cb166a1a38148b45a082f6d62b2d9446912bcf00
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-21 17:55:19 +0100

    Update gameType.ts

--- src/types/gameType.ts
+++ src/types/gameType.ts
@@ -2,9 +2,5 @@ import ruleType from "./ruleType";
 
 type gameType = { name: string; rules: ruleType[]; icon: string };
 
-export enum gameNames {
-  CLASSIC = "Classic",
-  BIG_BANG_THEORY = "Big Bang Theory",
-}
 
 export default gameType;
