dd04e092e4c24d6e3476de281c567f62ab179d07
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-24 19:43:14 +0100

    Delete getStatGameMode.ts

--- src/utils/getStatGameMode.ts
+++
@@ -1,6 +0,0 @@
-import appStates from "../constants/appStates";
-import getState from "./getState";
-
-const getStatGameMode = () => getState(appStates.STATCALCMODE);
-
-export default getStatGameMode;
