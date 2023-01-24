92787dee8667687dd20b2e438f69d6f25f01f95a
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-24 19:43:31 +0100

    Delete setThema.ts

--- src/utils/setThema.ts
+++
@@ -1,8 +0,0 @@
-import appStates from "../constants/appStates";
-import setState from "./setState";
-import { themas } from "../constants/themas";
-
-const setThema = (value: string) =>
-  setState(appStates.THEMA, value || Object.values(themas)[0]);
-
-export default setThema;
