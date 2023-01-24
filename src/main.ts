5ac95701e815f36e9c5d9020c31f2da86a204183
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-24 20:10:05 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,3 @@
 import GameContorller from "./modules/GameContorller";
-// import GameUI from "./modules/GameUI/GameUI";
-
-/* style */
-import "./style/reset.css";
-import "./style/style.css";
-
-import StateHandler from "./modules/StateHandler/StateHandler";
 
 new GameContorller();
-const state = new StateHandler({ mata: "faka" });
