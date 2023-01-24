d05e453ee5328625b52b393c66f686c42fddd383
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-24 19:43:25 +0100

    Delete setStatCalcMode.ts

--- src/utils/setStatCalcMode.ts
+++
@@ -1,8 +0,0 @@
-import appStates from "../constants/appStates";
-import setState from "./setState";
-import { statCalcModes } from "../constants/statCalcModes";
-
-const setStateCalcMode = (value: statCalcModes) =>
-  setState(appStates.STATCALCMODE, value);
-
-export default setStateCalcMode;
