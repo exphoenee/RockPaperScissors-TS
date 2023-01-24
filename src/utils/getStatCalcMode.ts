642a94e160b2a3f749aedb401339dc87348bbce0
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-24 19:43:10 +0100

    Delete getStatCalcMode.ts

--- src/utils/getStatCalcMode.ts
+++
@@ -1,6 +0,0 @@
-import appStates from "../constants/appStates";
-import getState from "./getState";
-
-const getStatCalcMode = () => getState(appStates.STATCALCMODE);
-
-export default getStatCalcMode;
