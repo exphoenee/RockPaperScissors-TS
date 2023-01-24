49588069a1a0e524d8c38135c1501339b9159b45
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-24 19:43:20 +0100

    Delete setGameMode.ts

--- src/utils/setGameMode.ts
+++
@@ -1,6 +0,0 @@
-import appStates from "../constants/appStates";
-import setState from "./setState";
-
-const getGameMode = (value: string) => setState(appStates.GAMEMODE, value);
-
-export default getGameMode;
