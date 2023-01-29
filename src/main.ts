6d6284fe74ed7fdf62569045d31bcdc4a3288fc5
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-29 17:41:16 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,3 +14,22 @@ const added = statisticsHandler.addValue({
 console.log(added);
 
 console.log(statisticsHandler.getStatistics());
+
+
+const added2 = statisticsHandler.addValue({
+  userName: "test",
+  gameName: "Cricket",
+  threwName: "Scissors",
+});
+
+console.log(added2);
+console.log(statisticsHandler.getStatistics());
+
+const added3 = statisticsHandler.addValue({
+  userName: "Lolla",
+  gameName: "Tucsok",
+  threwName: "Majom",
+});
+
+console.log(added3);
+console.log(statisticsHandler.getStatistics());
