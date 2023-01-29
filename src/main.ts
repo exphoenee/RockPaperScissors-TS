0809e29b651ebfed1d58f7287cb296d1321e02ea
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-29 17:34:24 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
 import GameContorller from "./modules/GameContorller";
+import StatisticsHandler from "./modules/StatisticsHandler/StatisticsHandler";
 
 new GameContorller();
+
+const statisticsHandler = new StatisticsHandler();
+
+const added = statisticsHandler.addValue({
+  userName: "test",
+  gameName: "Cricket",
+  threwName: "Scissors",
+});
+
+console.log(added);
+
+console.log(statisticsHandler.getStatistics());
