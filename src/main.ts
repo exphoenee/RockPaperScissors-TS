c5d962766f22309736a47447597dffa7279f1b46
exphoenee <bozzay.viktor@gmail.com>
2023-01-30 21:02:44 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,26 +2,3 @@ import GameContorller from "./modules/GameContorller";
 import StatisticsHandler from "./modules/StatisticsHandler/StatisticsHandler";
 
 new GameContorller();
-
-const statisticsHandler = new StatisticsHandler();
-
-statisticsHandler.addValue({
-  userName: "User1",
-  gameName: "Classic",
-  threwName: "Scissors",
-});
-statisticsHandler.addValue({
-  userName: "User1",
-  gameName: "Classic",
-  threwName: "Scissors",
-});
-statisticsHandler.addValue({
-  userName: "User2",
-  gameName: "Classic",
-  threwName: "Paper",
-});
-
-console.log("STATISTICS: ", statisticsHandler.getStatistics());
-
-statisticsHandler.getTable();
-statisticsHandler.getTableTransposed();
