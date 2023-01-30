bbefef938f97e91db93b8efec7d9ade1903c56d1
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-01-30 17:55:49 +0100

    Update StatisticsHandler.ts

--- src/modules/StatisticsHandler/StatisticsHandler.ts
+++ src/modules/StatisticsHandler/StatisticsHandler.ts
@@ -1,4 +1,8 @@
-import { gameStatisticsType } from "../../types/gameStatisticsType";
+import {
+  gameStatisticsType,
+  threwStatisticsType,
+  userStatisticsType,
+} from "../../types/gameStatisticsType";
 import { userNames } from "../../constants/userNames";
 import { gameNames } from "../../constants/gameNames";
 import { gameResultType } from "../../types/gameResultType";
@@ -11,17 +15,21 @@ class StatisticsHandler {
     if (statistics) this.statistics = statistics as gameStatisticsType;
   }
 
-  fillStatistics(statistics: gameStatisticsType) {
+  public fillStatistics(statistics: gameStatisticsType) {
     this.statistics = statistics;
   }
 
-  getThrewsFromGame(gameName: string) {
+  public getForStorage() {
+    return JSON.stringify(this.statistics);
+  }
+
+  private getThrewsFromGame(gameName: string) {
     const game = games.find((game) => game.name === gameName);
     if (!game) return [];
     return game.rules.map((game) => game.value);
   }
 
-  getTable(gameName = "Classic") {
+  public getTable(gameName = "Classic") {
     const gameStatistics = this.statistics.find(
       (game) => game.gameName === gameName
     );
@@ -29,35 +37,39 @@ class StatisticsHandler {
 
     if (!gameStatistics || !threws) return [];
 
-    const table = gameStatistics.statistics.reduce((acc, user) => {
-      const userName = user.userName;
+    const table: (string | number)[][] = gameStatistics.statistics.reduce(
+      (acc: (string | number)[][], user) => {
+        const threwValues = threws.map((threwName) =>
+          user.results
+            .filter(
+              (result) =>
+                result.threwName.toUpperCase() === threwName.toUpperCase()
+            )
+            .reduce((sum, result) => {
+              return sum + result.value;
+            }, 0)
+        );
 
-      const threwValues = threws.map((threwName) =>
-        user.results
-          .filter(
-            (result) =>
-              result.threwName.toUpperCase() === threwName.toUpperCase()
-          )
-          .reduce((sum, result) => {
-            return sum + result.value;
-          }, 0)
-      );
+        const total: number = threwValues.reduce(
+          (sum, value) => sum + value,
+          0
+        );
 
-      const total = threwValues.reduce((sum, value) => sum + value, 0);
-
-      acc.push([userName, ...threwValues, total]);
-      return acc;
-    }, []);
+        acc.push([user.userName, ...threwValues, total]);
+        return acc;
+      },
+      []
+    );
 
     const headerTitles = threws.map((threw) => {
       return threw.charAt(0).toUpperCase() + threw.slice(1).toLowerCase();
     });
     const headers = ["User", ...headerTitles, "Total"];
 
-    const total = table.reduce(
-      (acc, row) =>
+    const total: (string | number)[] = table.reduce(
+      (acc: number[], row) =>
         row.slice(1).reduce((_, value, i) => {
-          acc[i + 1] += value;
+          acc[i + 1] += +value;
           return acc;
         }, acc),
       [...Array(headers.length)].map(() => 0)
@@ -71,18 +83,18 @@ class StatisticsHandler {
     return table;
   }
 
-  getTableTransposed() {
+  public getTableTransposed() {
     const result = this.getTable();
     const transposed = result[0].map((_, i) => result.map((row) => row[i]));
     console.log(transposed);
     return transposed;
   }
 
-  getStatistics(): gameStatisticsType | [] {
+  public getStatistics(): gameStatisticsType | [] {
     return this.statistics;
   }
 
-  addValue({
+  public addValue({
     gameName,
     userName,
     threwName,
@@ -93,34 +105,37 @@ class StatisticsHandler {
   }) {
     const timeDate = new Date().toISOString();
 
-    const game = this.statistics?.find((game) => game.gameName === gameName);
-    if (!game) {
-      const newGameRecord = {
+    const sGame = this.statistics?.find((game) => game.gameName === gameName);
+    if (!sGame) {
+      const newGameRecord: gameStatisticsType = {
         gameName: gameName as gameNames,
         statistics: [
           {
             userName: userName as userNames,
-            results: [{ threwName, value: 1, timeDate }],
+            results: [{ threwName, value: 1, timeDate }] as threwStatisticsType,
           },
         ],
       };
       this.statistics.push(newGameRecord);
+
+      const sUser = sGame?.statistics?.find(
+        (user) => user.userName === userName
+      );
+      if (!sUser) {
+        const newUserRecord: userStatisticsType = {
+          userName: userName as userNames,
+          results: [{ threwName, value: 1, timeDate }] as threwStatisticsType,
+        };
+        sGame?.statistics?.push(newUserRecord);
+      }
+      const sThrew = sUser?.results.find(
+        (threw) => threw.threwName === threwName
+      );
+      sThrew
+        ? sThrew.value++
+        : sUser?.results.push({ threwName, value: 1, timeDate });
+      return true;
     }
-    const user = game?.statistics.find((user) => user.userName === userName);
-    if (!user) {
-      const newUserRecord = {
-        userName: userName as userNames,
-        results: [{ threwName, value: 1, timeDate }],
-      };
-      game?.statistics?.push(newUserRecord);
-    }
-    const threw = user?.results.find(
-      (result) => result.threwName === threwName
-    );
-    threw
-      ? threw.value++
-      : user?.results.push({ threwName, value: 1, timeDate });
-    return true;
   }
 }
 
