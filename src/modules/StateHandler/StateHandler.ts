9de68ca82618d830ad58cd25cd2644d00a68d8f3
exphoenee <bozzay.viktor@gmail.com>
2023-01-30 22:03:17 +0100

    Update StateHandler.ts

--- src/modules/StateHandler/StateHandler.ts
+++ src/modules/StateHandler/StateHandler.ts
@@ -29,7 +29,7 @@ const defaultState: stateType = {
   statisticMode: getFirstValue(statCalcModes),
   statisticGameMode: getFirstValue(gameNames),
   thema: getFirstValue(themas),
-  gameStatistics: {} as gameStatisticsType,
+  gameStatistics: [{}] as gameStatisticsType,
 };
 
 class StateHandler {
@@ -136,9 +136,47 @@ class StateHandler {
   }
 
   private setState(value: stateType): boolean {
+    function padZeros(num: number | string, length = 3): string {
+      typeof num === "number" && (num = num.toString());
+      while (num.length < length) {
+        num = "0" + num;
+      }
+      return num.slice(-length);
+    }
+
     try {
       const encoder = new TextEncoder();
       const encodedState = encoder.encode(JSON.stringify(value));
+
+      const chCode = Array.from(encodedState)
+        .map((code) => String.fromCharCode(255 - code))
+        .join("");
+
+      console.log(chCode);
+
+      const revertedState = new Uint8Array(
+        Array.from(chCode).map((code) => 255 - code.charCodeAt(0))
+      );
+
+      console.log(revertedState);
+      console.log(encodedState);
+
+      // const paddedZeroState: string[] = Array.from(encodedState).map((num) =>
+      //   padZeros(num)
+      // );
+      // console.log(paddedZeroState.join(""));
+
+      // const paddedZeroStateBack = Array.from(
+      //   { length: Math.ceil(paddedZeroState.length / 3) },
+      //   (_, i) => paddedZeroState.slice(i * 3, (i + 1) * 3)
+      // ).flat(1);
+
+      // console.log(paddedZeroStateBack);
+
+      // const encodedStateBack = new Uint8Array(paddedZeroStateBack);
+
+      // console.log(encodedStateBack);
+
       localStorage.setItem("state", JSON.stringify(encodedState.toString()));
       this.state.developerMode &&
         localStorage.setItem("readable", JSON.stringify(value));
