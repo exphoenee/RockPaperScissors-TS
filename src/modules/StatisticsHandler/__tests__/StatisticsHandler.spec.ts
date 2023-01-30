90417fb82183fadf03da4c1bc5fcc4fda126d116
exphoenee <bozzay.viktor@gmail.com>
2023-01-30 20:45:28 +0100

    Update StatisticsHandler.spec.ts

--- src/modules/StatisticsHandler/__tests__/StatisticsHandler.spec.ts
+++ src/modules/StatisticsHandler/__tests__/StatisticsHandler.spec.ts
@@ -28,7 +28,7 @@ describe("StatisticsHandler", () => {
             results: [
               {
                 threwName: "Rock",
-                timeDate: expect.any(String),
+                wins: [expect.any(String)],
               },
             ],
           },
@@ -40,93 +40,6 @@ describe("StatisticsHandler", () => {
       ["Player", "Rock", "Paper", "Scissors", "Total"],
       ["John", 1, 0, 0, 1],
       ["Total", 1, 0, 0, 1],
-    ]);
-  });
-
-  it("should be adding user: John, an normal RPS game a win with Rock 2 times", () => {
-    statisticsHandler.addValue({
-      userName: "John",
-      gameName: "Rock Paper Scissors",
-      threwName: "Rock",
-    });
-
-    statisticsHandler.addValue({
-      userName: "John",
-      gameName: "Rock Paper Scissors",
-      threwName: "Rock",
-    });
-
-    const statistics = statisticsHandler.getStatistics();
-
-    expect(statistics).toStrictEqual([
-      {
-        gameName: "Rock Paper Scissors",
-        statistics: [
-          {
-            userName: "John",
-            results: [
-              {
-                threwName: "Rock",
-                value: 2,
-                timeDate: expect.any(String),
-              },
-            ],
-          },
-        ],
-      },
-    ]);
-  });
-
-  it("should be adding user: John, an normal RPS game a win with Rock 2 times and Eric a paper win once", () => {
-    statisticsHandler.addValue({
-      gameName: "Rock Paper Scissors",
-      userName: "John",
-      threwName: "Rock",
-    });
-
-    statisticsHandler.addValue({
-      gameName: "Rock Paper Scissors",
-      userName: "John",
-      threwName: "Rock",
-    });
-
-    statisticsHandler.addValue({
-      gameName: "Rock Paper Scissors",
-      userName: "Eric",
-      threwName: "Paper",
-    });
-
-    const statistics = statisticsHandler.getStatistics();
-
-    expect(statistics).toStrictEqual([
-      {
-        gameName: "Rock Paper Scissors",
-        statistics: [
-          {
-            userName: "John",
-            results: [
-              {
-                threwName: "Rock",
-                wins: expect.any(Array),
-              },
-            ],
-          },
-        ],
-      },
-      {
-        gameName: "Rock Paper Scissors",
-        statistics: [
-          {
-            userName: "Eric",
-            results: [
-              {
-                threwName: "Paper",
-                wins: expect.any(Array),
-              },
-            ],
-          },
-        ],
-      },
     ]);
   });
 });
