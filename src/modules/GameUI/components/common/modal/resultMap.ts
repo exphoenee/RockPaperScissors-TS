67dedb155fd9865cfbf19d19a0ca0201bdd0047b
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-02-05 19:11:59 +0100

    Update resultMap.ts

--- src/modules/GameUI/components/common/modal/resultMap.ts
+++ src/modules/GameUI/components/common/modal/resultMap.ts
@@ -1,28 +1,15 @@
-import moduleName from "module";
-import { gameResults } from "../../../../../constants/gameResults";
-
 export type resultMapType = {
-  result: gameResults;
-  user: string;
-  opponent: string;
-  userChoice: string;
-  opponentChoice: string;
+  winner: string;
+  winsWith: string;
+  against: string;
 };
 
-const resultMap = ({
-  result,
-  user,
-  opponent,
-  userChoice,
-  opponentChoice,
-}: resultMapType) => {
-  const resultText = {
-    [gameResults.USER]: `${user} won with ${userChoice} against ${opponentChoice}`,
-    [gameResults.OPPONENT]: `${opponent} won with ${opponentChoice} against ${userChoice}`,
-    [gameResults.DRAW]: `It's a draw!`,
-  };
-
-  return `<p>${resultText[result]}</p>`;
+const resultMap = ({ winner, winsWith, against }: resultMapType) => {
+  return `<p>${
+    winner !== ""
+      ? `The winner is ${winner} with ${winsWith} against ${against}`
+      : "It's a draw!"
+  }</p>`;
 };
 
 export default resultMap;
