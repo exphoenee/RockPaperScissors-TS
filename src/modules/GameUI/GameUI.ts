39b0c11d4cdf292a446360b38868103d6a1d9feb
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-02-07 20:16:06 +0100

    Update GameUI.ts

--- src/modules/GameUI/GameUI.ts
+++ src/modules/GameUI/GameUI.ts
@@ -82,27 +82,31 @@ export default class GameUI {
     throw new Error("The setStatCalcMode is not defined");
   };
 
+  // TODO: at initialization must the scores and the statistics table also set
+
   public updateStaisticsTable = (tableDate: (number | string)[][]) => {
     // statisticTableMap(tableDate)
     const { modalBody: statModalBody } = this.getModal(modalNames.STAT);
 
     if (statModalBody) {
       const tableContainer = statModalBody.querySelector("#table-container");
-      tableContainer.innerHTML = "";
+      if (tableContainer) {
+        tableContainer.innerHTML = "";
 
-      domelemjs({
-        tag: "table",
-        attrs: { class: "statistics-table" },
-        parent: tableContainer,
-        children: tableDate.map((row) => {
-          return {
-            tag: "tr",
-            children: row.map((cell) => {
-              return { tag: "td", text: cell };
-            }),
-          };
-        }),
-      });
+        domelemjs({
+          tag: "table",
+          attrs: { class: "statistics-table" },
+          parent: tableContainer,
+          children: tableDate.map((row) => {
+            return {
+              tag: "tr",
+              children: row.map((cell) => {
+                return { tag: "td", text: cell };
+              }),
+            };
+          }),
+        });
+      }
     }
   };
 
