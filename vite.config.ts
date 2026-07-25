9301d028d99744536df3a5a7899e8e57d8cea593
exphoenee <bozzay.viktor@gmail.com>
2026-07-25 08:54:07 +0200

    fixed daployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // GitHub Pages alprojekt-útvonal: https://exphoenee.github.io/RockPaperScissors-TS/
+  base: "/RockPaperScissors-TS/",
   plugins: [],
 });
